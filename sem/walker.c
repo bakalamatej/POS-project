d95ac0fc19106bb9e47a3f50c76707e878afd11c
matej-pc <majetbakala@gmail.com>
2026-01-08 12:20:40 +0100

    wrap around fix in a world with obstacles

--- sem/walker.c
+++ sem/walker.c
@@ -19,17 +19,34 @@ void random_walk(SharedState *S, Walker* w)
     int new_x = w->x;
     int new_y = w->y;
 
+    // Urči základný smer pohybu (bez wrap-around)
     if (r < p_up)
-        new_y = (w->y - 1 + S->world_size) % S->world_size;
+        new_y = w->y - 1;
     else if (r < p_down)
-        new_y = (w->y + 1) % S->world_size;
+        new_y = w->y + 1;
     else if (r < p_left)
-        new_x = (w->x - 1 + S->world_size) % S->world_size;
+        new_x = w->x - 1;
     else
-        new_x = (w->x + 1) % S->world_size;
+        new_x = w->x + 1;
 
-    if (!S->use_obstacles || S->obstacles[new_y][new_x] == 0) {
-        w->x = new_x;
-        w->y = new_y;
+    // Pre svet BEZ prekážok: aplikuj wrap-around
+    if (!S->use_obstacles) {
+        new_x = (new_x + S->world_size) % S->world_size;
+        new_y = (new_y + S->world_size) % S->world_size;
+        
+        // Skontroluj prekážku (aj keď by nemali byť žiadne)
+        if (S->obstacles[new_y][new_x] == 0) {
+            w->x = new_x;
+            w->y = new_y;
+        }
+    } else {
+        // Pre svet S PREKÁŽKAMI: zostane na mieste pri hranici/prekážke
+        if (new_x >= 0 && new_x < S->world_size && 
+            new_y >= 0 && new_y < S->world_size &&
+            S->obstacles[new_y][new_x] == 0) {
+            w->x = new_x;
+            w->y = new_y;
+        }
+        // Inak zostane na pôvodnej pozícii
     }
 }
